298eec280b2b3c525525d4066127fa25303a6093
Manvir <manvir@Manvirs-MacBook-Pro.local>
2022-08-16 12:20:11 -0500

    First commit

---
+++ Numbers.java
@@ -0,0 +1,9 @@
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println("Printing numbers 1 to 10");
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
